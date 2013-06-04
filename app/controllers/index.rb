c4c4d1b6a6ca4983e1b2ab6269d781815590c9c1
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-04 15:15:00 -0500

    first commit

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,11 +5,20 @@ end
 
 # TODO: convert this route to use AJAX
 post '/rolls' do
+  puts params.inspect
+  # {'random' => nil}
   # If the user passes-in a "value", let's use it. Otherwise, we'll generate a random one.
   # See: roll_if_value_is_nil method in the Roll model.
   value = params[:value] ? params[:value].to_i : nil
-
+  puts value
+  
   @roll = value ? Roll.create({ value: value }) : Roll.create
 
-  erb :index  # HINT: what does this do? what should we do instead?
+  if request.xhr?
+    puts '**********'
+
+    erb :_die_roll, :layout => false
+  else
+    erb :index  # HINT: what does this do? what should we do instead?
+  end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -7,4 +7,17 @@ $(document).ready(function () {
   //   4- use jQuery to submit an AJAX post to the form's action
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
 
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    var randomnumber = Math.floor(Math.random()*6 +1);
+    console.log(randomnumber);
+    $.post('/rolls', {"value": randomnumber}, function(data){
+       console.log(data);
+
+      die_location = $('.container').find('#die').html(); 
+       // console.log(die_location);
+      $('#die').html(data);
+
+    });
+  });
 });
